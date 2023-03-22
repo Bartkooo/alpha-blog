9ffa1b891500ce57273cdf735ae7055470182fff
Bartkooo <bartosz.plochocki@gmail.com>
2023-03-22 15:53:13 +0100

    user signup view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root "pages#home"
-  get "about", to: "pages#about"
+  root 'pages#home'
+  get 'about', to: 'pages#about'
   resources :articles
   #[:show, :index, :new, :create, :edit, :update, :destroy]
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
