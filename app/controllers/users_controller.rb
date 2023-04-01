3c34a4a765e6282692018295c1e796a59a7fd43a
Bartkooo <bartosz.plochocki@gmail.com>
2023-04-01 14:17:46 +0200

    delete user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_user, only: [:edit, :update]
-  before_action :require_appropriate_user, only: [:edit, :update]
+  before_action :require_appropriate_user, only: [:edit, :update, :destroy]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 4)
@@ -36,6 +36,13 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "#{@user.username} profile and #{@user.articles.length} articles successfully deleted."
+    redirect_to articles_path
   end
 
   private
