97a0a0f6b6870e327adaaed09060a458b248015c
Bartkooo <bartosz.plochocki@gmail.com>
2023-05-16 20:59:49 +0200

    create Comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_04_111039) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_16_185914) do
   create_table "article_categories", force: :cascade do |t|
     t.integer "article_id"
     t.integer "category_id"
@@ -26,6 +26,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_04_111039) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "article_id"
+    t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
