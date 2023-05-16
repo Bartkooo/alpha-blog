f49543fa2c22d8d006f76e9e1fb1474033df82bd
Bartkooo <bartosz.plochocki@gmail.com>
2023-05-16 21:33:04 +0200

    add comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,18 @@
-class CommentsController < ApplicationControll:error
-  
+class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.article_id = params[:article_id]
+
+    @comment.save
+
+    redirect_to article_path(@comment.article)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles
   #[:show, :index, :new, :create, :edit, :update, :destroy]
+  resources :articles do
+    resources :comments
+  end
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
   get 'login', to: 'sessions#new'
